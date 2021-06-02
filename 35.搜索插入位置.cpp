2e49195483fd87c8757dddfbdcd89c3cee90230d
guozhe <guozhe@meituan.com>
2021-06-03 01:04:14 +0800

    2021/6/3 Binary Search

--- 35.搜索插入位置.cpp
+++ 35.搜索插入位置.cpp
@@ -7,9 +7,21 @@
 // @lc code=start
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-
+  int searchInsert(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+    int ans = nums.size();
+    while (left <= right) {
+      int mid = (right - left) / 2 + left;
+      if (target <= nums[mid]) {
+        ans = mid;
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
     }
+    return ans;
+  }
 };
 // @lc code=end
 

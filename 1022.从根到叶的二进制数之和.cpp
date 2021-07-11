51f9a95a8913df910e4b5b158f786fe9b61633a0
guozhe <dylanguo916@gmail.com>
2021-07-11 17:47:59 +0800

    2021/7/11

--- 1022.从根到叶的二进制数之和.cpp
+++ 1022.从根到叶的二进制数之和.cpp
@@ -18,9 +18,36 @@
  */
 class Solution {
 public:
-    int sumRootToLeaf(TreeNode* root) {
-      
+  int res = 0;
+  int sumRootToLeaf(TreeNode* root) {
+    vector<int> path;
+    dfs(root, path);
+    return res;
+  }
+  void dfs(TreeNode *root, vector<int>& path) {
+    if (!root)
+      return;
+    path.push_back(root->val);
+    if (!root->left && !root->right) {
+      res += cal(path);
+      return;
     }
+    if (root->left) {
+      dfs(root->left, path);
+      path.pop_back();
+    }
+    if (root->right) {
+      dfs(root->right, path);
+      path.pop_back();
+    }
+  }
+  int cal(const vector<int>& path) {
+    int sum = 0;
+    for (int i = path.size() - 1; i >= 0; --i) {
+      sum += path[i]* pow(2, path.size() - 1 - i);
+    }
+    return sum;
+  }
 };
 // @lc code=end
 

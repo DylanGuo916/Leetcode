8c21c5bf363acb66a6bba1da51af516f1d3a4cb0
guozhe <dylanguo916@gmail.com>
2021-07-11 18:12:18 +0800

    2021/7/11

--- 283.移动零.cpp
+++ 283.移动零.cpp
@@ -7,9 +7,15 @@
 // @lc code=start
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-
+  void moveZeroes(vector<int> &nums) {
+    int n = nums.size(), left = 0, right = 0;
+    while (right < n) {
+      if (nums[right]) {
+        swap(nums[left], nums[right]);
+        left++;
+      }
+      right++;
     }
+  }
 };
 // @lc code=end
-

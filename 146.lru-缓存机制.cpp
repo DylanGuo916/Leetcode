63b2b606e9fe1f7198669282f7ee617a85f31fe7
guozhe <dylanguo916@gmail.com>
2021-08-14 17:17:44 +0800

    Update 146.lru-缓存机制.cpp

--- 146.lru-缓存机制.cpp
+++ 146.lru-缓存机制.cpp
@@ -8,16 +8,42 @@
 class LRUCache {
 public:
   LRUCache(int capacity) {
-    
+    m_capacity = capacity;
   }
   
   int get(int key) {
-
+    const auto it = m_hashtable.find(key);
+    // If key does not exist
+    if (it == m_hashtable.cend()) return -1;
+    // Move this key to the front of the list
+    m_cache.splice(m_cache.begin(), m_cache, it->second);
+    return it->second->second;
   }
   
   void put(int key, int value) {
+    const auto it = m_shashtable.find(key);
+    // key already exists
+    if (it != m_hashtable.cend()) {
+      it->second->second = value;
+      m_cache.splice(m_cache.begin(), m_cache, it->second);
+      return;
+    }
 
+    // key does not exist 
+    // reached the capacity, remove the oldest element
+    if (m_cache.size() == m_capacity) {
+      const auto& node = m_cache.back();
+      m_hashtable.erase(node.first);
+      m_cache.pop_back();
+    }
+
+    m_cache.emplace_front(key, value);
+    m_hashtable[key] = m_cache.begin();
   }
+private:
+  int m_capacity;
+  list<pair<int, int>> m_cache;
+  unordered_map<int, list<pair<int, int>>::iterator> m_hashtable;
 };
 
 /**

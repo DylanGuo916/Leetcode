c6f2e7465cb07257547e98cae3a005fffe2e5cdc
guozhe <dylanguo916@gmail.com>
2021-08-16 19:29:43 +0800

    2021/8/16

--- 450.删除二叉搜索树中的节点.cpp
+++ 450.删除二叉搜索树中的节点.cpp
@@ -19,8 +19,26 @@
 class Solution {
 public:
   TreeNode *deleteNode(TreeNode *root, int key) {
-    
+    if (root == nullptr) return root;
+    if (key > root->val) {
+      root->right = deleteNode(root->right, key);
+    } else if (key < root->val) {
+      root->left = deleteNode(root->left, key);
+    } else {
+      if (root->left != nullptr && root->right != nullptr) {
+        TreeNode* min = root->right;
+        // 找到右子树的最小节点，与根结点交换
+        while (min->left != nullptr) min = min->left;
+        root->val = min->val;
+        root->right = deleteNode(root->right, min->val);
+      } else {
+        TreeNode* new_root = root->left == nullptr ? root->right : root->left;
+        root->left = root->right = nullptr;
+        delete root;
+        return new_root;
+      }
+    }
+    return root;
   }
 };
 // @lc code=end
-
